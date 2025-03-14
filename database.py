1328f916d452e035d26b30d83afa560f49853425
akopjandvd <akopjandvd@gmail.com>
2025-03-15 00:25:49 +0100

    Remove check same thread

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./todo.db")  # Használja a környezeti változót
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
