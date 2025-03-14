b6583112ab553ad90ae7dfb75e2c6544ae598d8f
akopjandvd <akopjandvd@gmail.com>
2025-03-14 22:09:32 +0100

    update database testing in ci

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./todo.db")  # Használja a környezeti változót
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
